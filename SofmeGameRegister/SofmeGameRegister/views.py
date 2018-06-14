f5d45d73103786a69c410ae3008e4aafe0d3021f
すー <s1732087ap@s.chibakoudai.jp>
2018-06-15 01:49:04 +0900

    view page

--- SofmeGameRegister/SofmeGameRegister/views.py
+++ SofmeGameRegister/SofmeGameRegister/views.py
@@ -4,9 +4,3 @@ from django.views.generic import TemplateView
 from .models import *
 
 
-class FormView(TemplateView):
-    template_name = "form.html"
-
-    def get(self, request, *args, **kwargs):
-        context = super(WorkerListView, self).get_context_data(**kwargs)
-        return render(self.request, self.template_name, context)
