e97476f6d752786d230dcb65397a453060397889
すー <s1732087ap@s.chibakoudai.jp>
2018-06-14 23:58:18 +0900

    add temp

--- SofmeGameRegister/SofmeGameRegister/settings.py
+++ SofmeGameRegister/SofmeGameRegister/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'form',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -85,15 +86,15 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- SofmeGameRegister/SofmeGameRegister/urls.py
+++ SofmeGameRegister/SofmeGameRegister/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    #url(r'^form/', manager_view.FormView.as_view())
 ]
