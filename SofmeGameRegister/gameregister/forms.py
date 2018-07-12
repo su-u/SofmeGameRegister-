c36e9693dd92fdf78a3e361a29224d250ea8b80e
すー <s1732087ap@s.chibakoudai.jp>
2018-07-12 14:14:32 +0900

    input file　変更

--- SofmeGameRegister/gameregister/forms.py
+++ SofmeGameRegister/gameregister/forms.py
@@ -5,3 +5,9 @@ class GameInfoForm(forms.ModelForm):
     class Meta:
         model = GameInfo
         fields = ["name", "representative", "game_id", "discription", "gamefile", "panel", "movie"]
+
+        widgets = {
+            "gamefile": forms.TextInput(attrs={"type":"file","onchange": "$('#fake_text_box').val($(this).val())", "id": "gamefile",
+                                               "style": "display:none;"})
+        }
+
