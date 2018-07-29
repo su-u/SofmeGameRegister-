2d04bb72a843e5fac17a5502c0a9ffdc92d00eef
すー <s1732087ap@s.chibakoudai.jp>
2018-07-30 01:31:37 +0900

    update lp

--- SofmeGameRegister/gameregister/urls.py
+++ SofmeGameRegister/gameregister/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url("^register$", views.GameInfoView, name = "GameInfoView"),
     url(r"^complete", views.complete, name = "complete"),
     url(r"^edit/(?P<editing_id>\d+)/$", views.edit, name="edit"),
-    url(r"^$", views.index, name="index"),
-    url(r"^lp$", views.lp, name = "lp"),
+    url(r"^index$", views.index, name="index"),
+    url(r"^$", views.lp, name = "lp"),
 ]
