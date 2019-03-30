32c81f8430d1c666d5b85eda921fb1c797b9efc3
すー <aaaiiiuuu35@gmail.com>
2019-03-30 22:51:22 +0900

    add utils

---
+++ SofmeGameRegister/gameregister/utils.py
@@ -0,0 +1,39 @@
+
+def construct_change_message(form, formsets, add):
+    """
+    Construct a JSON structure describing changes from a changed object.
+    Translations are deactivated so that strings are stored untranslated.
+    Translation happens later on LogEntry access.
+    """
+    change_message = []
+    if add:
+        change_message.append({'added': {}})
+    elif form.changed_data:
+        change_message.append({'changed': {'fields': form.changed_data}})
+
+    if formsets:
+        with translation_override(None):
+            for formset in formsets:
+                for added_object in formset.new_objects:
+                    change_message.append({
+                        'added': {
+                            'name': str(added_object._meta.verbose_name),
+                            'object': str(added_object),
+                        }
+                    })
+                for changed_object, changed_fields in formset.changed_objects:
+                    change_message.append({
+                        'changed': {
+                            'name': str(changed_object._meta.verbose_name),
+                            'object': str(changed_object),
+                            'fields': changed_fields,
+                        }
+                    })
+                for deleted_object in formset.deleted_objects:
+                    change_message.append({
+                        'deleted': {
+                            'name': str(deleted_object._meta.verbose_name),
+                            'object': str(deleted_object),
+                        }
+                    })
+    return change_message
